949520ba6ec37b42e1943cc5eb9f1c6b6a852087
darkworldchamp <140893545+darkworldchamp@users.noreply.github.com>
2024-06-24 16:34:35 +0530

    Add files via upload

---
+++ 1_Storage.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ErrorHandling {
+
+    uint public balance;
+
+    constructor(uint initial_balance) {
+
+        require(initial_balance >= 1000, "initial balance must be greater than or equal to 1000");
+        balance = initial_balance;
+    }
+
+    function withdraw(uint amount) public {
+        require(amount <= balance, "balance cannot be less than amount");
+        balance -= amount;
+    }
+    function assertFunction() public view{
+        assert(balance >= 1000); 
+        // Should never be false, if it is, there's a bug
+        // ...
+    }
+
+    error low_balance(uint balance, uint amount);
+    function revertFunction(uint _amount) public view{
+        uint _balance = address(this).balance;
+        if (_amount> _balance) {
+            revert low_balance({balance: _balance,amount: _amount});
+        }
+       
+    }
+}
